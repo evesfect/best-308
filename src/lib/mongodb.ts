b93704c1ee80861e77f30d61b444ba48827812c5
Semih702 <semih.dogan1@sabanciuniv.edu>
2024-12-02 01:15:10 +0300

    completed the migration of gridfs to remote database.

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -1,23 +1,41 @@
 import mongoose from 'mongoose';
+import { GridFSBucket } from 'mongodb';
 
 const MONGO_URI = "mongodb+srv://root:rpassword@cluster0.rz8bd.mongodb.net/e-commerce?retryWrites=true&w=majority";
 
 if (!process.env.MONGO_URI) {
-  throw new Error('Please define the MONGO_URI environment variable');
+    throw new Error('Please define the MONGO_URI environment variable');
 }
 
 declare global {
-  var mongooseConnection: Promise<typeof mongoose> | undefined;
+    var mongooseConnection: Promise<typeof mongoose> | undefined;
+    var gridFSBucket: GridFSBucket | undefined;
 }
+
+let gridFSBucket: GridFSBucket | undefined;
 
 if (!global.mongooseConnection) {
-  global.mongooseConnection = mongoose.connect(MONGO_URI).then((mongooseInstance) => {
-    console.log("MongoDB connected successfully");
-    return mongooseInstance;
-  }).catch((err) => {
-    console.error("Error connecting to MongoDB:", err);
-    throw err;
-  });
+    global.mongooseConnection = mongoose
+        .connect(MONGO_URI)
+        .then((mongooseInstance) => {
+            console.log("MongoDB connected successfully");
+
+            if (!global.gridFSBucket) {
+                global.gridFSBucket = new GridFSBucket(mongooseInstance.connection.db, {
+                    bucketName: 'product_images', // Updated bucket name
+                });
+                console.log("GridFSBucket initialized with bucketName: product_images");
+            }
+
+            return mongooseInstance;
+        })
+        .catch((err) => {
+            console.error("Error connecting to MongoDB:", err);
+            throw err;
+        });
 }
 
+gridFSBucket = global.gridFSBucket;
+
+export { gridFSBucket };
 export default global.mongooseConnection;
