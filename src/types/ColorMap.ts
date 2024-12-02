2e4b1af51cc8af1576e1b9d803fa6a42fda4fea6
Burak Ala <burak.ala@sabanciuniv.edu>
2024-12-03 00:55:17 +0300

    added colors properly

---
+++ src/types/ColorMap.ts
@@ -0,0 +1,11 @@
+const colorMap: {[key: string]: string }= {
+    'denim blue': 'rgb(21, 96, 159)',
+    'dark slate gray': 'rgb(47, 79, 79)',
+    'tomato': 'rgb(255, 99, 71)',
+    'medium sea green': 'rgb(60, 179, 113)',
+    'firebrick': 'rgb(178, 34, 34)',
+    'slate blue': 'rgb(106, 90, 205)',
+    // Add more color mappings as needed
+};
+
+export default colorMap;
