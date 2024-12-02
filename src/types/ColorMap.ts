3ecca8eafb08f788e21fa9229e359a27b960ecd2
Semih702 <semih.dogan1@sabanciuniv.edu>
2024-12-03 01:17:46 +0300

    added new colors

--- src/types/ColorMap.ts
+++ src/types/ColorMap.ts
@@ -5,6 +5,16 @@ const colorMap: {[key: string]: string }= {
     'medium sea green': 'rgb(60, 179, 113)',
     'firebrick': 'rgb(178, 34, 34)',
     'slate blue': 'rgb(106, 90, 205)',
+    'brown': 'rgb(89, 51, 29)',
+    'blue': 'rgb(116, 179, 206)',
+    'black': 'rgb(0, 0, 0)',
+    'gray': 'rgb(128, 128, 128)',
+    'khaki': 'rgb(195, 176, 145)',
+    'olive': 'rgb(80, 80, 00)',
+    'navy': 'rgb(208, 208, 128)',
+    'green': 'rgb(23, 65, 33)',
+    'red': 'rgb(210, 31, 60)',
+
     // Add more color mappings as needed
 };
 
